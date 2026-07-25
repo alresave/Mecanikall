dc7bb71a9ce672a253d6c9c03a47f91a1e621bd3
Alberto Gonzalez <alresave@gmail.com>
2026-07-25 09:43:58 -0600

    perf: lazy load application routes
    perf: lazy load application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,15 @@
 import { Routes } from '@angular/router';
-import { SolicitudTicketComponent } from './components/solicitud-ticket/solicitud-ticket.component';
-import { PanelTallerComponent } from './components/panel-taller/panel-taller.component';
-import { AccesoTallerComponent } from './components/acceso-taller/acceso-taller.component';
-import { PanelAdminComponent } from './components/panel-admin/panel-admin.component';
-import { PanelRefaccionesComponent } from './components/panel-refacciones/panel-refacciones.component';
-import { ReporteSuscripcionesComponent } from './components/reporte-suscripciones/reporte-suscripciones.component';
-import { OperacionEntidadesComponent } from './components/operacion-entidades/operacion-entidades.component';
-import { SuscripcionComponent } from './components/suscripcion/suscripcion.component';
-import { ComisionesComponent } from './components/comisiones/comisiones.component';
 import { tallerGuard } from './auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: SolicitudTicketComponent, title: 'Mecanikall | Solicitar ayuda' },
-  { path: 'acceso', component: AccesoTallerComponent, title: 'Mecanikall | Acceso de talleres' },
-  { path: 'taller', component: PanelTallerComponent, canActivate: [tallerGuard], title: 'Mecanikall | Panel de talleres' },
-  { path: 'admin', component: PanelAdminComponent, canActivate: [tallerGuard], title: 'Mecanikall | Administración' },
-  { path: 'refacciones', component: PanelRefaccionesComponent, canActivate: [tallerGuard], title: 'Mecanikall | Refacciones' },
-  { path: 'reportes', component: ReporteSuscripcionesComponent, canActivate: [tallerGuard], title: 'Mecanikall | Prospectos' },
-  { path: 'operacion', component: OperacionEntidadesComponent, canActivate: [tallerGuard], title: 'Mecanikall | Operación' },
-  { path: 'suscripcion', component: SuscripcionComponent, canActivate: [tallerGuard], title: 'Mecanikall | Suscripción' },
-  { path: 'comisiones', component: ComisionesComponent, canActivate: [tallerGuard], title: 'Mecanikall | Comisiones' },
+  { path: '', loadComponent: () => import('./components/solicitud-ticket/solicitud-ticket.component').then(m => m.SolicitudTicketComponent), title: 'Mecanikall | Solicitar ayuda' },
+  { path: 'acceso', loadComponent: () => import('./components/acceso-taller/acceso-taller.component').then(m => m.AccesoTallerComponent), title: 'Mecanikall | Acceso de talleres' },
+  { path: 'taller', loadComponent: () => import('./components/panel-taller/panel-taller.component').then(m => m.PanelTallerComponent), canActivate: [tallerGuard], title: 'Mecanikall | Panel de talleres' },
+  { path: 'admin', loadComponent: () => import('./components/panel-admin/panel-admin.component').then(m => m.PanelAdminComponent), canActivate: [tallerGuard], title: 'Mecanikall | Administración' },
+  { path: 'refacciones', loadComponent: () => import('./components/panel-refacciones/panel-refacciones.component').then(m => m.PanelRefaccionesComponent), canActivate: [tallerGuard], title: 'Mecanikall | Refacciones' },
+  { path: 'reportes', loadComponent: () => import('./components/reporte-suscripciones/reporte-suscripciones.component').then(m => m.ReporteSuscripcionesComponent), canActivate: [tallerGuard], title: 'Mecanikall | Prospectos' },
+  { path: 'operacion', loadComponent: () => import('./components/operacion-entidades/operacion-entidades.component').then(m => m.OperacionEntidadesComponent), canActivate: [tallerGuard], title: 'Mecanikall | Operación' },
+  { path: 'suscripcion', loadComponent: () => import('./components/suscripcion/suscripcion.component').then(m => m.SuscripcionComponent), canActivate: [tallerGuard], title: 'Mecanikall | Suscripción' },
+  { path: 'comisiones', loadComponent: () => import('./components/comisiones/comisiones.component').then(m => m.ComisionesComponent), canActivate: [tallerGuard], title: 'Mecanikall | Comisiones' },
   { path: '**', redirectTo: '' },
 ];
